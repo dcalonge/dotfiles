82894c342f42f26e85fba8e2335e734f8ef4df4e
daniel <dcalonge01@gmail.com>
2024-02-11 18:27:39 +0100

    hyprland,fish shell and gtklock

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    neofetch
+	# Commands to run in interactive sessions can go here
+end
+
+alias dnfu='sudo dnf upgrade'
+alias dnfi='sudo dnf install'
+alias dnfr='sudo dnf remove'
+alias ls='eza -a --color=always --group-directories-first'
