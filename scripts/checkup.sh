87b39abe26668059bfce43a793e49e6c2473540d
dcalonge <dcalonge01@gmail.com>
2024-07-17 12:35:54 +0200

    edit scripts

--- scripts/checkup.sh
+++ scripts/checkup.sh
@@ -4,7 +4,7 @@
 arch_updates=$(paru -Qu | wc -l)
 
 # Verificar actualizaciones de aplicaciones flatpak
-flatpak_updates=$(/usr/bin/flatpak remote-ls --columns=application -a --updates)
+flatpak_updates=$(/usr/bin/flatpak remote-ls --columns=application -a --updates | wc -l)
 
 num_updates=$((arch_updates + flatpak_updates))
 
