83a70356deff162fe63e1cc5eb5d4bf46f7f7302
dcalonge <dcalonge01@gmail.com>
2024-09-10 18:43:19 +0200

    update

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -12,4 +12,5 @@ done >/dev/null 2>&1
 systemctl --user set-environment XDG_CURRENT_DESKTOP=sway
 systemctl --user import-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 
-exec hash dbus-update-activation-environment 2>/dev/null && dbus-update-activation-environment --systemd DISPLAY XDG_CURRENT_DESKTOP=sway WAYLAND_DISPLAY
+hash dbus-update-activation-environment 2>/dev/null
+exec dbus-update-activation-environment --systemd DISPLAY XDG_CURRENT_DESKTOP=sway WAYLAND_DISPLAY
