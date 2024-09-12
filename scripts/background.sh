a67684cf45329cb35e4b3b53f1264e4f44c9789d
dcalonge <dcalonge01@gmail.com>
2024-09-12 17:10:21 +0200

    update

---
+++ scripts/background.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+WALLPAPER_DIR="/home/daniel/Pictures/wallpapers"
+RANDOM_IMAGE=$(find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \) | shuf -n 1)
+
+cp "$RANDOM_IMAGE" /tmp/wallpaper.jpg
+
+killall swaybg && swaybg -i /tmp/wallpaper.jpg
