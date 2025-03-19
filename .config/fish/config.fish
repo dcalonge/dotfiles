1cd88c4d33a020ef583081a36ca3d4d28d941de2
Daniel Calonge <dcalonge01@gmail.com>
2025-03-19 19:15:58 +0100

    added river
    added river

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,18 @@
 fortune -s | cowsay -f tux
 
 
-abbr -a pu 'sudo pacman -Syu'
-abbr -a pi 'sudo pacman -S'
-abbr -a pr 'sudo pacman -Rns'
-abbr -a ps 'pacman -Ss'
-abbr -a au paru
-abbr -a ai 'paru -S'
-abbr -a ar 'paru -Rns'
-abbr -a as 'paru -Ss'
+abbr -a fu 'flatpak update'
+abbr -a fi 'flatpak install'
+abbr -a fr 'flatpak uninstall'
+abbr -a fs 'flatpak search'
+abbr -a pu 'doas dnf upgrade'
+abbr -a pi 'doas dnf install'
+abbr -a pr 'doas dnf remove'
+abbr -a ps 'dnf search'
 abbr -a m 'sudo make clean install; rm -f config.h'
 abbr -a v nvim
+abbr -a sudo doas
+
 
 alias ls='eza -a --color=always --icons --group-directories-first'
 alias cat='bat'
